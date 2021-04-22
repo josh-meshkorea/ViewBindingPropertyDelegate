bdbce66bce6c43d377b9635939061c7a16011d35
seunghee.park <seunghee.park@meshkorea.net>
2021-04-22 16:16:27 +0900

    ci(gradle): deploy to mesh nexus

--- vbpd/vbpd-full/src/main/java/by/kirich1409/viewbindingdelegate/AnyLazyBindings.kt
+++ vbpd/vbpd-full/src/main/java/by/kirich1409/viewbindingdelegate/AnyLazyBindings.kt
@@ -8,10 +8,22 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 import by.kirich1409.viewbindingdelegate.internal.ViewBindingCache
 
+/**
+ * Create new [ViewBinding] associated with the [Any]
+ *
+ * @param T Class of expected [ViewBinding] result class
+ * @param viewProvider Provide a [View] from the [Any].
+ */
 @JvmName("lazyViewBindingAny")
 public inline fun <reified T : ViewBinding> Any.lazyViewBinding(crossinline viewProvider: (Any) -> View) =
     lazyViewBinding(T::class.java) { viewProvider(it) }
 
+/**
+ * Create new [ViewBinding] associated with the [Any]
+ *
+ * @param viewBindingClass Class of expected [ViewBinding] result class
+ * @param viewProvider Provide a [View] from the [Any].
+ */
 @JvmName("lazyViewBindingAny")
 public fun <T : ViewBinding> Any.lazyViewBinding(
     viewBindingClass: Class<T>,
